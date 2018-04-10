48d5ea432731da2772fa4203ca028d0b8cabf38c
rijii52 <alikdayan@gmail.com>
2018-04-10 20:51:49 +0300

    Very top app structure prepared

--- App.js
+++ App.js
@@ -1,58 +1,332 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
 import {
   Platform,
   StyleSheet,
   Text,
-  View
+  View,
+  TouchableHighlight,
+  StatusBar
 } from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+class TopControlBlock extends Component {
+  constructor(props) {
+    super(props);
 
-type Props = {};
-export default class App extends Component<Props> {
-  render() {
+    this.onStartTaraining = this.onStartTaraining.bind(this);
+    this.onFinishTaraining = this.onFinishTaraining.bind(this);
+    this.onStoreTaraining = this.onStoreTaraining.bind(this);
+
+    this.styles = StyleSheet.create({
+      theblock: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderBottomStyle: 'solid',
+        borderBottomWidth: 1,
+        borderBottomColor: '#CFD8DC',
+      },
+      touch: {
+        width: '50%'
+      },
+      button: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        borderStyle: 'solid',
+        borderWidth: 1,
+        borderColor: 'white',
+        borderRadius: 5,
+        padding: 10,
+        backgroundColor: '#455A64'
+      },
+      buttontext: {
+        fontSize: 20,
+        color: 'white',
+      },
+      disbuttontext: {
+        fontSize: 20,
+        color: 'gray',
+      }
+    });
+  }
+
+  onStartTaraining () {
+    this.props.startTaraining();
+  }
+
+  onFinishTaraining () {
+    this.props.finishTaraining();
+  }
+
+  onStoreTaraining () {
+    this.props.storeTaraining();
+  }
+
+  getButton () {
+    switch (this.props.status) {
+      case "initial":
+        return (
+          <TouchableHighlight onPress={this.onStartTaraining} style={this.styles.touch}>
+            <View style={this.styles.button}>
+              <Text style={this.styles.buttontext}>Start Training</Text>
+            </View>
+          </TouchableHighlight>
+        );
+        
+      case "training":
+        if (this.props.runningStatus === "walking") {
+          return  (
+            <TouchableHighlight onPress={this.onFinishTaraining} style={this.styles.touch}>
+              <View style={this.styles.button}>
+                <Text style={this.styles.buttontext}>Finish Training</Text>
+              </View>
+            </TouchableHighlight>
+            );
+          }
+        else {
+          return  (
+            <TouchableHighlight style={this.styles.touch}>
+              <View style={this.styles.button}>
+                <Text style={this.styles.disbuttontext}>Store Training</Text>
+              </View>
+            </TouchableHighlight>
+          );
+        }
+
+      case "pending_store":
+        return (
+          <TouchableHighlight onPress={this.onStoreTaraining} style={this.styles.touch}>
+            <View style={this.styles.button}>
+              <Text style={this.styles.buttontext}>Store Training</Text>
+            </View>
+          </TouchableHighlight>
+        );
+
+      default:
+        break;
+    }
+  }
+
+  render () {
+    var Button = this.getButton ();
+
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
+      <View style={this.styles.theblock}>
+        {Button}
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+class WelcomeBlock extends Component {
+
+constructor(props) {
+  super(props);
+
+  this.styles = StyleSheet.create({
+    theblock: {
+      flex: 5,
+      justifyContent: 'center',
+      alignItems: 'center'
+    },
+    welcometext: {
+      fontSize: 20,
+      color: 'white'
+    }
+  });
+}
+
+  render () {
+    return (
+      <View style={this.styles.theblock}>
+        <Text style={this.styles.welcometext}>Welcome to Baritzakala!</Text>
+      </View>
+    );
+  }
+}
+
+class ActiveTraining extends Component {
+  constructor(props) {
+    super(props);
+
+    this.onRunning = this.onRunning.bind(this);
+    this.onWalking = this.onWalking.bind(this);
+
+    this.styles = StyleSheet.create({
+      theblock: {
+        flex: 5,
+        justifyContent: 'center',
+        alignItems: 'center'
+      },
+      touch: {
+        width: '50%'
+      },
+      button: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        borderStyle: 'solid',
+        borderWidth: 1,
+        borderColor: 'white',
+        borderRadius: 5,
+        padding: 10,
+        backgroundColor: '#455A64'
+      },
+      buttontext: {
+        fontSize: 20,
+        color: 'white',
+      }      
+    });
+  }
+
+  onRunning () {
+    this.props.updateRunningStatus("running");
+  }
+
+  onWalking () {
+    this.props.updateRunningStatus("walking");
+  }
+
+  getButton () {
+    if (this.props.runningStatus === "running") {
+      return (
+        <TouchableHighlight style={this.styles.touch} onPress={this.onWalking}>
+          <View style={this.styles.button}>
+            <Text style={this.styles.buttontext}>Stop Race</Text>
+          </View>
+        </TouchableHighlight>
+      );
+    }
+    else {
+      return (
+        <TouchableHighlight style={this.styles.touch} onPress={this.onRunning}>
+          <View style={this.styles.button}>
+            <Text style={this.styles.buttontext}>Start Race</Text>
+          </View>
+        </TouchableHighlight>
+      );
+    }
+  }
+
+  render () {
+
+    var button = this.getButton();
+
+    return (
+      <View style={this.styles.theblock}>
+        {button}
+      </View>
+    );
+  }
+}
+
+class FinishedTraining extends Component {
+  constructor(props) {
+    super(props);
+
+    this.styles = StyleSheet.create({
+      theblock: {
+        flex: 5,
+        justifyContent: 'center',
+        alignItems: 'center'
+      },
+      finishtext: {
+        fontSize: 20,
+        color: 'white'
+      }
+    });
+  }
+
+  render () {
+    return (
+      <View style={this.styles.theblock}>
+        <Text style={this.styles.finishtext}>training was finished</Text>
+      </View>
+    );
+  }
+}
+
+export default class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      status: "initial"
+    };
+
+    this.startTaraining = this.startTaraining.bind(this);
+    this.updateRunningStatus = this.updateRunningStatus.bind(this);
+    this.finishTaraining = this.finishTaraining.bind(this);
+    this.storeTaraining = this.storeTaraining.bind(this);
+
+    this.styles = StyleSheet.create({
+      appcontainer: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'stretch',
+        backgroundColor: '#607D8B'
+      }
+    });
+  }
+
+  startTaraining () {
+    this.setState({
+      status: "training",
+      runningStatus: "walking"
+    });
+  }
+
+  updateRunningStatus (runningStatus) {
+    this.setState ({
+      runningStatus: runningStatus
+    });
+  }
+
+  finishTaraining () {
+    this.setState({
+      status: "pending_store"
+    });
+  }
+
+  storeTaraining () {
+    this.setState({
+      status: "initial"
+    });
+  }
+
+  getContentBlock () {
+    switch (this.state.status) {
+      case "initial":
+        return (
+          <WelcomeBlock />
+        );
+        
+      case "training":
+        return (
+          <ActiveTraining runningStatus={this.state.runningStatus} updateRunningStatus={this.updateRunningStatus} />
+        );
+
+      case "pending_store":
+        return (
+          <FinishedTraining />
+        );
+
+      default:
+        break;
+    }
+  }
+
+  render() {
+
+    var contentBlock = this.getContentBlock();
+
+    return (
+      <View style={this.styles.appcontainer}>
+        <StatusBar backgroundColor="#37474F" barStyle="light-content" hidden={false} />
+        <TopControlBlock status={this.state.status} runningStatus={this.state.runningStatus} 
+          startTaraining={this.startTaraining} finishTaraining={this.finishTaraining} storeTaraining={this.storeTaraining} />
+        {contentBlock}
+      </View>
+    );
+  }
+}
